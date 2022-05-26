0d1259618dd22932d6eda26a6618269923e8a35a
Mateus Borges Ferreira <mateuferrei@hotmail.com>
2022-05-26 18:16:02 -0300

    adicionando estrutura inicial do projeto

---
+++ get.js
@@ -0,0 +1,62 @@
+ get = () => {
+
+    const config = {
+        params: {
+            _limit: 2
+        }
+    };
+    const teste = axios.get('http://localhost:8080/adote-quatropatas/pet', config)
+        .then((response) => response.data.forEach(element => {
+            let linha = criaLinha(element);
+            tabela.appendChild(linha);
+
+        }))
+    console.log('FREZAA PQ VC MATOU O KURIRIN')
+
+return teste;
+
+}
+
+
+function criaLinha(data) {
+
+    linha = document.createElement("tr");
+    tdNome = document.createElement("td");
+    tdIdade = document.createElement("td");
+    tdEspecie = document.createElement("td");
+    tdPorte = document.createElement("td");
+    tdGenero = document.createElement("td");
+    tdPeso = document.createElement("td");
+    tdCidade = document.createElement("td");
+    tdEstado = document.createElement("td");
+    tdDeficiencia = document.createElement("td");
+    //button = document.createElement("button");
+
+    tdNome.innerHTML = data.nome;
+    tdIdade.innerHTML = data.idade;
+    tdEspecie.innerHTML = data.especie;
+    tdPorte.innerHTML = data.porte;
+    tdGenero.innerHTML = data.genero;
+    tdPeso.innerHTML = data.peso;
+    tdCidade.innerHTML = data.cidade;
+    tdEstado.innerHTML = data.estado;
+    tdDeficiencia.innerHTML = data.deficiencia;
+
+    linha.appendChild(tdNome);
+    linha.appendChild(tdIdade);
+    linha.appendChild(tdEspecie);
+    linha.appendChild(tdPorte);
+    linha.appendChild(tdGenero);
+    linha.appendChild(tdPeso);
+    linha.appendChild(tdCidade);
+    linha.appendChild(tdEstado);
+    linha.appendChild(tdDeficiencia);
+
+
+    return linha;
+}
+
+
+
+ 
+
---
+++ post.js
@@ -0,0 +1,55 @@
+    const post = (body) => {
+        const data = {
+            nome: body.nome,
+            idade: body.idade,
+            especie: body.especie,
+            genero: body.genero,
+            porte: body.porte,
+            peso: body.peso,
+            cidade: body.cidade,
+            estado: body.estado,
+            deficiencia: body.deficiencia
+          };
+    
+        axios.post('http://localhost:8080/adote-quatropatas/pet', data)
+        .then ((response)=> renderOutput(response))
+        console.log('socorro Deus')
+    }
+
+
+function main() {
+    event.preventDefault();
+    
+    let nome = document.getElementById("nome").value
+    let idade = document.getElementById("idade").value
+    let especie = document.getElementById("especie").value
+    let genero = document.getElementById("genero").value
+    let porte = document.getElementById("porte").value
+    let peso = document.getElementById("peso").value
+    let cidade = document.getElementById("cidade").value
+    let estado = document.getElementById("estado").value
+    let deficiencia = document.getElementById("deficiencia").value
+    console.log(nome)
+    console.log(idade)
+    console.log(especie)
+    console.log(genero)
+    console.log(porte)
+    console.log(peso)
+    console.log(estado)
+    console.log(cidade)
+    console.log(deficiencia)
+
+    body = {
+        "nome": nome,
+        "idade": idade,
+        "especie": especie,
+        "genero": genero,
+        "porte": porte,
+        "peso": peso,
+        "estado": estado,
+        "cidade": cidade,
+        "deficiencia": deficiencia
+    }
+
+    post(body)
+}
